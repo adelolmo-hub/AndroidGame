328192b47d7092f79104323b68157b79381f4c6d
cfgs <albert.delolmo@gmail.com>
2022-05-12 19:29:26 +0200

    Inicio de sesion casi terminado

--- app/src/main/java/com/example/cardgameproject/MainActivity.java
+++ app/src/main/java/com/example/cardgameproject/MainActivity.java
@@ -4,44 +4,86 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.InputType;
 import android.text.TextUtils;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase database;
+    private SQLiteCardDB sqliteHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sqliteHelper = new SQLiteCardDB(this);
+
+        database = sqliteHelper.getWritableDatabase();
+
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle("Nombre jugador");
         alert.setMessage("Por favor introduzca su nombre");
-// Set an EditText view to get user input
+        // Set an EditText view to get user input
         final EditText user = new EditText(this);
         final EditText password = new EditText(this);
+        final Button createAccount = new Button( this);
         password.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
         alert.setView(user);
         alert.setView(password);
         alert.setPositiveButton("Ok", (dialog, whichButton) -> {
-            String userName;
-            String userPass;
-            if (!TextUtils.isEmpty(user.getText())) {
-                userName = user.getText().toString();
+            boolean userEmpty = isEmpty(user,getString(R.string.userEmpty));
+            boolean passwordEmpty = isEmpty(password, getString(R.string.passwordEmpty));
 
-            }else{
-                Toast.makeText(getApplicationContext(), "User name can't be empty", Toast.LENGTH_SHORT).show();
+            if(userEmpty || passwordEmpty){
+                return;
             }
-            if (!TextUtils.isEmpty(password.getText())) {
-                userPass = password.getText().toString();
 
-            }else{
-                Toast.makeText(getApplicationContext(), "User password can't be empty", Toast.LENGTH_SHORT).show();
-            }
+            compareUserPassword(user.getText().toString(), password.getText().toString());
+
             //TODO - COMPROBAR EN BASE DE DATOS QUE EL USUARIO Y EL PASSWORD SON CORRECTOS Y PASAR A LA SIGUIENTE ACTIVITY
         });
+    }
+
+    private void compareUserPassword(String user, String password) {
+
+        String[] columns = new String[]{DatabaseContract.UsersTable.COLUMN_NAME, DatabaseContract.UsersTable.COLUMN_PASSWORD};
+        String[] selectionArgs = new String[]{user};
+
+        Cursor c = database.query(DatabaseContract.UsersTable.TABLE, columns, DatabaseContract.UsersTable.COLUMN_NAME + "=?", selectionArgs, null, null,
+                null);
+
+        if(c.moveToFirst()){
+            String passwordFromDatabase = c.getString(c.getColumnIndexOrThrow(DatabaseContract.UsersTable.COLUMN_PASSWORD));
+            if(passwordFromDatabase.equals(password)){
+                //TODO - Iniciar sesion
+                Toast.makeText(this, "Iniciar sesion", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, R.string.wrong_password, Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            //TODO - El usuario no existe no esta hecho
+            Toast.makeText(this, "No existe", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    private boolean isEmpty(EditText editText, String errorMsg) {
+        boolean empty = TextUtils.isEmpty(editText.getText());
+        if (empty) {
+            Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_SHORT).show();
+        }
+        return empty;
     }
 }
