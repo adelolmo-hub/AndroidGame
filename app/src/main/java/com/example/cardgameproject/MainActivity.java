f9499da91b7951e7d14e5b535d935c212a13e916
cfgs <albert.delolmo@gmail.com>
2022-05-16 19:26:10 +0200

    Crear cuenta terminado

--- app/src/main/java/com/example/cardgameproject/MainActivity.java
+++ app/src/main/java/com/example/cardgameproject/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.InputType;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteCardDB sqliteHelper;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,66 +36,102 @@ public class MainActivity extends AppCompatActivity {
 
         database = sqliteHelper.getWritableDatabase();
 
-        AlertDialog.Builder alertLogIn = new AlertDialog.Builder(this);
-        alertLogIn.setTitle("Log in");
-        // Set an EditText view to get user input
+        AlertDialog.Builder alertLogInBuilder = new AlertDialog.Builder(this);
+        alertLogInBuilder.setTitle("Log in");
+
+        //Set a Layout
         LinearLayout layout = new LinearLayout(this);
-        final EditText user = new EditText(this);
-        final EditText password = new EditText(this);
-        password.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
         layout.setOrientation(LinearLayout.VERTICAL);
-        user.setPadding(20, 20, 20, 20);
-        password.setPadding(20, 20, 20, 20);
+
+        //Set EditText view to get user input
+        final EditText user = new EditText(this);
+        user.setHint("User");
+        final EditText password = new EditText(this);
+        password.setHint("Password");
+        password.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        //Set a button to create an account
+        final Button createAccountButton = new Button(this);
+        createAccountButton.setText("Register");
+        createAccountButton.setOnClickListener(v -> alertDialogCreateAccount());
+
         layout.addView(user);
         layout.addView(password);
-        alertLogIn.setView(layout);
-        alertLogIn.setPositiveButton("Ok", (dialog, whichButton) -> {
-            boolean userEmpty = isEmpty(user,getString(R.string.userEmpty));
-            boolean passwordEmpty = isEmpty(password, getString(R.string.passwordEmpty));
+        layout.addView(createAccountButton);
 
-            if(userEmpty || passwordEmpty){
-                return;
-            }
+        alertLogInBuilder.setView(layout);
+        alertLogInBuilder.setPositiveButton("OK", null);
+        alertLogInBuilder.setNegativeButton("Close", null);
 
-            compareUserPassword(user.getText().toString(), password.getText().toString());
+        final AlertDialog alertLogIn = alertLogInBuilder.create();
+        alertLogIn.setOnShowListener(dialog -> {
+            Button button = alertLogIn.getButton(AlertDialog.BUTTON_POSITIVE);
+            button.setOnClickListener(v -> {
+                boolean userEmpty = isEmpty(user,getString(R.string.userEmpty));
+                boolean passwordEmpty = isEmpty(password, getString(R.string.passwordEmpty));
 
-            //TODO - COMPROBAR EN BASE DE DATOS QUE EL USUARIO Y EL PASSWORD SON CORRECTOS Y PASAR A LA SIGUIENTE ACTIVITY
+                if(!userEmpty && !passwordEmpty){
+                    compareUserPassword(user.getText().toString(), password.getText().toString());
+                    alertLogIn.dismiss();
+                }
+
+                //TODO - COMPROBAR EN BASE DE DATOS QUE EL USUARIO Y EL PASSWORD SON CORRECTOS Y PASAR A LA SIGUIENTE ACTIVITY
+            });
         });
+
         alertLogIn.show();
 
-        alertLogIn.setPositiveButton("Create Account", (dialog, whichButton) -> {
+    }
 
-            AlertDialog.Builder alertCreateAccount = new AlertDialog.Builder(this);
-            alertCreateAccount.setTitle("Create Account");
-            // Set an EditText view to get user input
-            final EditText userCreate = new EditText(this);
-            final EditText passwordCreate = new EditText(this);
-            final EditText passwordValidation = new EditText(this);
 
-            passwordCreate.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
-            passwordValidation.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+    private void alertDialogCreateAccount(){
+        AlertDialog.Builder createAccountBuilder = new AlertDialog.Builder(this);
+        createAccountBuilder.setTitle("Create Account");
 
-            alertCreateAccount.setView(userCreate);
-            alertCreateAccount.setView(passwordCreate);
-            alertCreateAccount.setView(passwordValidation);
-            alertCreateAccount.setPositiveButton("Ok", (dialogCreate, whichButtonCreate) -> {
+        createAccountBuilder.setPositiveButton("Ok", null);
+        createAccountBuilder.setNegativeButton("Close", null);
+        //Set a Layout
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        // Set an EditText view to get user input
+        final EditText userCreate = new EditText(this);
+        userCreate.setHint("User");
+        final EditText passwordCreate = new EditText(this);
+        passwordCreate.setHint("Password");
+        final EditText passwordValidation = new EditText(this);
+        passwordValidation.setHint("Repeat your password");
+
+        passwordCreate.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        passwordValidation.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        layout.addView(userCreate);
+        layout.addView(passwordCreate);
+        layout.addView(passwordValidation);
+
+        createAccountBuilder.setView(layout);
+
+        final AlertDialog dialogCreateAccount = createAccountBuilder.create();
+        dialogCreateAccount.setOnShowListener(dialog -> {
+            Button button = dialogCreateAccount.getButton(AlertDialog.BUTTON_POSITIVE);
+            button.setOnClickListener(v -> {
                 boolean userEmpty = isEmpty(userCreate,getString(R.string.userEmpty));
                 boolean passwordEmpty = isEmpty(passwordCreate, getString(R.string.passwordEmpty));
                 boolean passwordValidationEmpty = isEmpty(passwordValidation, getString(R.string.passwordEmpty));
 
-                if(!userEmpty || !passwordEmpty || !passwordValidationEmpty){
+                if(!userEmpty && !passwordEmpty && !passwordValidationEmpty){
                     if (passwordCreate.getText().toString().equals(passwordValidation.getText().toString())) {
-                        createAccount(user.getText().toString(), password.getText().toString());
+                        createAccount(userCreate.getText().toString(), passwordCreate.getText().toString());
+                        Toast.makeText(this, "Cuenta creada", Toast.LENGTH_SHORT).show();
                     }else {
                         passwordValidation.setError("Password doesn't match, try again");
                     }
                 }
                 //TODO - PASAR A LA SIGUIENTE ACTIVITY
             });
-            alertCreateAccount.show();
         });
+        dialogCreateAccount.show();
     }
-
     private void compareUserPassword(String user, String password) {
 
         String[] columns = new String[]{DatabaseContract.UsersTable.COLUMN_NAME, DatabaseContract.UsersTable.COLUMN_PASSWORD};
@@ -141,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean isEmpty(EditText editText, String errorMsg) {
         boolean empty = TextUtils.isEmpty(editText.getText());
         if (empty) {
-            Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_SHORT).show();
+            editText.setError(errorMsg);
         }
         return empty;
     }
