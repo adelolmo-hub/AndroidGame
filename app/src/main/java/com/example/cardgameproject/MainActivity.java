3b0b815680ac212141a7c8ebf35c1b40df1eab68
Albert <albert.ga.qu@gmail.com>
2022-05-17 21:21:43 +0200

    Menu juego in development.

--- app/src/main/java/com/example/cardgameproject/MainActivity.java
+++ app/src/main/java/com/example/cardgameproject/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         sqliteHelper = new SQLiteCardDB(this);
 
         database = sqliteHelper.getWritableDatabase();
